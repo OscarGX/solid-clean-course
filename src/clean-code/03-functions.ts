0a512163680f32075da53f90c6fc161cc43852fb
OscarGX <reyesmartinezoscar3g15@gmail.com>
2024-10-24 21:14:26 -0600

    Capítulo 17 terminado

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,46 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieActors( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBiography(actorId: string ) {
+        console.log({ actorId });
+    }
+    
+    interface Movie {
+        cast: string[];
+        description: string;
+        rating: string;
+        title: string;
+    }
+
+    // Crear una película
+    function createMovie({ title, description, rating, cast }: Movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
